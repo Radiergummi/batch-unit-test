fe60ae35ad356cc0de95ea6a41e569082870a681
Mo <Radiergummi@users.noreply.github.com>
2016-02-18 16:30:56 +0100

    Correctly escaped all code examples within echo statements

--- tests/example.test.cmd
+++ tests/example.test.cmd
@@ -9,21 +9,21 @@ echo Using its error level, you can give feedback to the main test process
 echo about the test outcome.
 echo This could look like so:
 echo.
-echo.    if %%commandOutput%% == 0 (
+echo.    if %%commandOutput%% == 0 ^(
 echo.      echo Test 4/14: Pass.
-echo.    ) else (
+echo.    ^) else ^(
 echo.      echo Test 4/14: FAILED! Expected 0 bot got %%commandOutput%%.
-echo.      set %failed%=true
-echo.    )
+echo.      set %%failed%%=true
+echo.    ^)
 echo.
 echo In the end, you should manage to keep track of failed tests and return 
 echo an error level. Every value greater than 0 is taken as a sign the test 
 echo experienced an error. This could look like so:
 echo.
-echo.    if %failed% == true (
+echo.    if %%failed%% == true ^(
 echo.      exit /b 1
-echo.    ) else (
+echo.    ^) else ^(
 echo.      exit /b 0
-echo.    )
+echo.    ^)
 
 exit /b 0
