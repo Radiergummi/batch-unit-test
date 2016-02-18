402eaa1b3329992dce48e34c5915faa81e6b4e91
Mo <Radiergummi@users.noreply.github.com>
2016-02-18 16:23:32 +0100

    Create iDontCountAsATestFile.cmd

---
+++ tests/iDontCountAsATestFile.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+echo Hi there! This file is not treated as a test module. Test modules must end in ".test.cmd".
+echo Every other script in this folder is simply ignored, so you could put your helper or
+echo fixture scripts in here without problems.
